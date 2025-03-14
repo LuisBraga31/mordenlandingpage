84b63b87253626f476fc4ea01eb94d0114cba2a5
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2025-03-14 15:13:29 -0300

    Criando componente About

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { Hero } from "./components/hero"
+import { Hero } from "./components/hero";
+import { About } from "./components/about"
 
 export default function Home() {
 
   return (
     <main>
       <Hero/>
+      <About/>
     </main>
   )
 
