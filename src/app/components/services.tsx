d4717d931a4bde965f90458249817ced351358f6
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2025-03-20 14:36:07 -0300

    Finalizando pagina de services

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -49,20 +49,30 @@ export function Services() {
         }
     })
 
+    function scrollPrev() {
+        emblaApi?.scrollPrev();
+    }
+
+    function scrollNext() {
+        emblaApi?.scrollNext();
+    }
+
+
+
     return (
         <section className="bg-white py-16">
             <div className="container mx-auto px-4">
-                <div className="">
-                    <h2 className="text-4xl font-bold mb-12"> Serviços </h2>
+                
+                <h2 className="text-4xl font-bold mb-12"> Serviços </h2>
 
-                    <div className="relative">
+                <div className="relative">
                         
-                        <div className="overflow-hidden" ref={emblaRef}>
-                            <div className="flex">
-                                {services.map((item, index) => (
-                                    <div className='flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3' key={index}>
-                                        <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col'>
-                                            <div className="flex-1 flex items-start justify-between">
+                    <div className="overflow-hidden" ref={emblaRef}>
+                        <div className="flex">
+                            {services.map((item, index) => (
+                                <div className='flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3' key={index}>
+                                    <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full flex flex-col'>
+                                        <div className="flex-1 flex items-start justify-between">
                                                 <div>
                                                     <span className='text-3xl'> {item.icon} </span>
                                                     <div>
@@ -72,28 +82,38 @@ export function Services() {
                                                         </p>
                                                     </div>
                                                 </div>
+                                        </div>
+
+                                        <div className='border-t border-gray-700 pt-4 flex items-center justify-between'>
+                                            <div className="flex items-center gap-2 text-sm">
+                                                <Clock className='w-4 h-4'/>
+                                                <span> {item.duration} </span>
                                             </div>
 
-                                            <div className='border-t border-gray-700 pt-4 flex items-center justify-between'>
-                                                <div className="flex items-center gap-2 text-sm">
-                                                    <Clock className='w-4 h-4'/>
-                                                    <span> {item.duration} </span>
-                                                </div>
+                                            <a href="#" className='flex items-center justify-center gap-2 hover:bg-red-500 py-1 px-4 rounded-md duration-300'>
+                                                <WhatsappLogo className='w-4 h-4'/>
+                                                Entrar em Contato
+                                            </a>
 
-                                                <a href="#" className='flex items-center justify-center gap-2 hover:bg-red-500 py-1 px-4 rounded-md duration-300'>
-                                                    <WhatsappLogo className='w-4 h-4'/>
-                                                    Entrar em Contato
-                                                </a>
+                                        </div>
 
-                                            </div>
-
-                                        </article>
-                                    </div>
-                                ))}
-                            </div>
+                                    </article>
+                                </div>
+                            ))}
                         </div>
-
                     </div>
+
+                    <button
+                        className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-2 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
+                        onClick={scrollPrev}>
+                        <ChevronLeft className='w-6 h-6 text-gray-600' />
+                    </button>
+
+                    <button
+                        className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-7 -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
+                        onClick={scrollNext}>
+                        <ChevronRight className='w-6 h-6 text-gray-600' />
+                    </button>
 
                 </div>
 
