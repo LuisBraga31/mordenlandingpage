a3403d4a166994a2d6b90554ef6d3984e1089a12
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2025-03-20 16:10:56 -0300

    Criando componente footer

---
+++ src/app/components/footer.tsx
@@ -0,0 +1,27 @@
+import { FacebookLogo, InstagramLogo, YoutubeLogo } from '@phosphor-icons/react/dist/ssr'
+import Image from 'next/image'
+
+import golden from '../../../public/golden.png'
+import royal from '../../../public/royal.png'
+import primier from '../../../public/primier.png'
+import whiskas from '../../../public/whiskas.png'
+import natural from '../../../public/natural.png'
+
+const brands = [
+    { name: "Royal Canin", logo: royal },
+    { name: "Golden", logo: golden },
+    { name: "Primier", logo: primier },
+    { name: "Formula Natural", logo: natural },
+    { name: "Whiskas", logo: whiskas },
+    { name: "Golden", logo: golden },
+]
+
+export function Footer () {
+
+    return (
+        <section>
+
+        </section>
+    )
+
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Hero } from "./components/hero";
 import { About } from "./components/about"
 import { Services } from "./components/services";
 import { Testimonials } from "./components/testimonials";
+import { Footer } from "./components/footer";
 
 export default function Home() {
 
@@ -11,6 +12,7 @@ export default function Home() {
       <About/>
       <Services/>
       <Testimonials/>
+      <Footer/>
     </main>
   )
 
