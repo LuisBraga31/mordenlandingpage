b1edcf7f5471e9c40bba7aa6602ba3f08cd18537
Luis Gustavo Braga <luisgustavobsilva@gmail.com>
2025-03-20 16:25:00 -0300

    Finalizando footer

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -19,8 +19,72 @@ const brands = [
 export function Footer () {
 
     return (
-        <section>
+        <section className='bg-[#E84C3D] pt-10 pb-6 text-white'>
+            <div className="container px-4 mx-auto">
+                <div className="border-b border-white/20 pb-8">
+                    <h4 className="text-3xl font-semibold mb-8 text-center"> Marcas que trabalhamos </h4>
 
+                    <div className='grid grid-cols-2 lg:grid-cols-6 gap-8'>
+                        {brands.map((item, index) => (
+                            <div key={index} className='bg-white p-4 rounded-lg flex items-center justify-center'>
+                                <Image
+                                    src={item.logo}
+                                    alt={item.name}
+                                    width={100}
+                                    height={50}
+                                    style={{
+                                        width: "auto",
+                                        height: "auto"
+                                    }}
+                                    className='object-contain'
+                                />
+                            </div>
+                        ))}
+                    </div>
+
+                </div>
+
+                <footer className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-12 mt-5'>
+                    
+                    <div>
+                        <h3 className='text-2xl font-semibold mb-2'>Pet Shop Dev </h3>
+                        
+                        <p className='mb-4'> Cuidando do seu melhor amigo com amor e dedicação. </p>
+                        <a href="#" className='bg-green-500 px-4 py-2 rounded-md'>
+                            Contato via WhatsApp
+                        </a>
+
+                    </div>
+
+                    <div>
+                        <h3 className='text-2xl font-semibold mb-2'>Contatos</h3>
+                        
+                        <p>Email: teste@teste.com</p>
+                        <p>Telefone: (XX)91234-1234</p>
+                        <p>Rua X, centro, Itajuba | MG</p>
+
+                    </div>
+
+                    <div>
+                        <h3 className='text-2xl font-semibold mb-2'>Redes sociais</h3>
+                        
+                        <div className='flex gap-4'>
+                            <a href="#" target='_blank'>
+                                <FacebookLogo className='w-8 h-8' />
+                            </a>
+                            <a href="#" target='_blank'>
+                                <InstagramLogo className='w-8 h-8' />
+                            </a>
+                            <a href="#" target='_blank'>
+                                <YoutubeLogo className='w-8 h-8' />
+                            </a>
+
+                        </div>
+                    </div>
+
+                </footer>
+
+            </div>
         </section>
     )
 
